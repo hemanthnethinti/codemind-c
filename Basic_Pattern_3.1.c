22a541680d8ac2a9f67becd399070d9f544178e1
hemanthnethinti <147475853+hemanthnethinti@users.noreply.github.com>
2023-12-09 23:30:17 +0530

    #include<stdio.h> int main () {     int n,i,j;     scanf("%d",&n);     for(j=n;j>=1;j--)     {         for(i=j;i>=1;i--)         {             printf("* ");
            }printf("
    ");
        }
    }

---
+++ Basic_Pattern_3.1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(j=n;j>=1;j--)
+    {
+        for(i=j;i>=1;i--)
+        {
+            printf("* ");
+            
+        }printf("
+");
+    }
+}
