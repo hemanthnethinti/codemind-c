8e549f7db4480c9effd7f4b03097f02c456d7613
hemanthnethinti <147475853+hemanthnethinti@users.noreply.github.com>
2023-12-17 16:11:58 +0530

    #include<stdio.h> int main () {     int i,n,g,ar[100],c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {      scanf("%d",&ar[i]);      if(i%2==0)      c+=ar[i];     }      printf("%d",c); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    int i,n,g,ar[100],c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+     scanf("%d",&ar[i]);
+     if(i%2==0)
+     c+=ar[i];
+    }
+     printf("%d",c);
+}
