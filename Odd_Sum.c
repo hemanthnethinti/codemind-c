e1dced8d3b78114c48bf20002043c74a67f13c68
hemanthnethinti <147475853+hemanthnethinti@users.noreply.github.com>
2023-12-17 14:22:20 +0530

    #include<stdio.h> int main () {     int n,ar[100],sum=0;     scanf("%d",&n);     for(int i =0;i<n;i++)     {         scanf("%d",&ar[i]);         if(ar[i]%2==1)         sum+=ar[i];     }
        printf("%d",sum);
    }

---
+++ Odd_Sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int n,ar[100],sum=0;
+    scanf("%d",&n);
+    for(int i =0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+        if(ar[i]%2==1)
+        sum+=ar[i];
+    }
+        
+    printf("%d",sum);
+}
