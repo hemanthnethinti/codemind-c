aea51ae935fba7c232bc9372c0ce435cf2f5d53c
hemanthnethinti <147475853+hemanthnethinti@users.noreply.github.com>
2023-12-19 10:51:20 +0530

    #include<stdio.h> int main () {     int n,c=0,i;     scanf("%d",&n);     int ar[n];     for(i=0;i<n;i++)     {         scanf("%d",&ar[i]);     }     for(i=0;i<n;i++)     {         if(ar[i]%2==1&&i%2==1)         c++;     }if(c>=n/2)     printf("True");     else     printf("False"); }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int n,c=0,i;
+    scanf("%d",&n);
+    int ar[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(ar[i]%2==1&&i%2==1)
+        c++;
+    }if(c>=n/2)
+    printf("True");
+    else 
+    printf("False");
+}
